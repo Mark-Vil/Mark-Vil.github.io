367b436c0e0f926a247fa45f5b3e924f8c004101
Mark-Vil <114152202+Mark-Vil@users.noreply.github.com>
2022-12-08 20:06:23 +0800

    Added first website

---
+++ index.js
@@ -0,0 +1,22 @@
+function Mail() {
+    var params = {
+        from_name: document.getElementById("Name").value,
+        Subject: document.getElementById("Subject").value,
+        Email: document.getElementById("Email").value,
+        Message: document.getElementById("Message").value,
+    };
+    const serviceID = "service_2w5savg";
+    const templateID = "template_gb5cnmq";
+
+    emailjs.send(serviceID, templateID, params).then((res) => {
+        document.getElementById("Name").value = "";
+        document.getElementById("Subject").value = "";
+        document.getElementById("Email").value = "";
+        document.getElementById("Message").value = "";
+        console.log(res);
+        alert("Your message sent succesfully");
+    })
+        .catch((err) => console.log(err));
+}    
+        
+    
