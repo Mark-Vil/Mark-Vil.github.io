80a3a85bceb0b6dfe4b17ec478533a8e70439d33
Mark-Vil <114152202+Mark-Vil@users.noreply.github.com>
2023-02-24 22:35:58 +0800

    hj

--- public/loader.js
+++ public/loader.js
@@ -2,7 +2,7 @@ const loaderIframe = document.getElementById('loader-iframe');
 loaderIframe.addEventListener('load', function() {
   const loaderStyles = document.createElement('link');
   loaderStyles.rel = 'stylesheet';
-  loaderStyles.href = 'loader.css';
+  loaderStyles.href = 'loading.css';
   loaderIframe.contentDocument.head.appendChild(loaderStyles);
 });
 
